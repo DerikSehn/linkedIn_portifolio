050503ecdb426b1bf8196c17b82876cfcaea09b7
Derik <derikbosing@gmail.com>
2023-12-12 23:32:02 -0300

    Ajuste de padding

--- src/components/timeline/feed/PostActions.tsx
+++ src/components/timeline/feed/PostActions.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 
 const PostActions = () => {
   return (
     <div className="p-3 flex">
-      <div className="flex justify-center items-center space-x-4">
-        <div className="flex p-2 px-3 hover:bg-zinc-200  transition-colors items-center cursor-pointer text-sm  font-bold gap-2 text-zinc-500 max-w-[200px] animate-duration-150 justify-around">
+      <div className="flex justify-center items-center ">
+        <div className="flex p-2  hover:bg-zinc-200  transition-colors items-center cursor-pointer text-sm  font-bold gap-2 text-zinc-500 max-w-[200px] animate-duration-150 justify-around">
           <span className='material-icons text-zinc-500'>
             thumb_up
           </span>
@@ -12,7 +11,7 @@ const PostActions = () => {
           Gostei
           </span>
         </div>
-        <div className="flex p-2 px-3 hover:bg-zinc-200  transition-colors items-center cursor-pointer text-sm  font-bold gap-2 text-zinc-500 max-w-[200px] animate-duration-150 justify-around">
+        <div className="flex p-2  hover:bg-zinc-200  transition-colors items-center cursor-pointer text-sm  font-bold gap-2 text-zinc-500 max-w-[200px] animate-duration-150 justify-around">
           <span className='material-icons text-zinc-500'>
             message
           </span>
@@ -20,7 +19,7 @@ const PostActions = () => {
           Comentar
           </span>
         </div>
-        <div className="flex p-2 px-3 hover:bg-zinc-200  transition-colors items-center cursor-pointer text-sm  font-bold gap-2 text-zinc-500 max-w-[200px] animate-duration-150 justify-around">
+        <div className="flex p-2  hover:bg-zinc-200  transition-colors items-center cursor-pointer text-sm  font-bold gap-2 text-zinc-500 max-w-[200px] animate-duration-150 justify-around">
           <span className='material-icons text-zinc-500 '>
             repeat
           </span>
@@ -28,7 +27,7 @@ const PostActions = () => {
           Compartilhar
           </span>
         </div> 
-        <div className="flex p-2 px-3 hover:bg-zinc-200  transition-colors items-center cursor-pointer text-sm  font-bold gap-2 text-zinc-500 max-w-[200px] animate-duration-150 justify-around">
+        <div className="flex p-2  hover:bg-zinc-200  transition-colors items-center cursor-pointer text-sm  font-bold gap-2 text-zinc-500 max-w-[200px] animate-duration-150 justify-around">
           <span className='material-icons text-zinc-500 -rotate-[50deg] translate-x-2'>
             send
           </span>
